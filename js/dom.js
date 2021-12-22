2aa70cbfaa8b28de20570f5d466bfe065435f373
Md. arifur Rahman <coder.roby@gmail.com>
2021-12-22 08:37:16 +0600

    finding element, tag , data from form

---
+++ js/dom.js
@@ -0,0 +1,12 @@
+const x = document.forms['frm1'];
+//console.dir(x[0].value);
+const len = x.length;
+
+let text = " ";
+for(let i = 0; i < len; i++ ){
+    text += "<p>" + x[i].value + "</p>"; 
+    
+    console.log(x[i].value);
+}
+
+document.getElementById('formValues').innerHTML = text;
