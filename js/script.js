ca1ab9d479ede6f0f6343a62bfe4c5ba76b3fc01
Md. arifur Rahman <coder.roby@gmail.com>
2021-12-23 22:02:43 +0600

    work on form validation.

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ function formValidation(){
     //console.log(formData[0].value);
     const input1Value = formData.elements[0].value;
     if(isNaN(input1Value) &&  typeof input1Value === 'string' && format.test(input1Value) == false ) console.log(input1Value);
-    else alert('Please input String value')
-    return false;
+    else alert('Please input String value');
+
+    return true;
 }
