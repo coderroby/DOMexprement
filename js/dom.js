de2196d63bae8d54d0f158d9206c2cb5e1f92afa
Md. arifur Rahman <coder.roby@gmail.com>
2021-12-25 04:48:37 +0600

    change position o Box DOM

--- js/dom.js
+++ js/dom.js
@@ -1,4 +1,4 @@
-const x = document.forms['frm1'];
+/* const x = document.forms['frm1'];
 //console.dir(x[0].value);
 const len = x.length;
 
@@ -9,4 +9,23 @@ for(let i = 0; i < len; i++ ){
     console.log(x[i].value);
 }
 
-document.getElementById('formValues').innerHTML = text;
+document.getElementById('formValues').innerHTML = text; */
+
+
+//animation
+function changeMe() {
+    const animateFrame = document.getElementById("animate");
+    console.log(animateFrame);
+    let mypos = 0;
+    const startChange = setInterval(change, 20);
+    function change() {
+        mypos++;
+        if (mypos < 380) {
+            animateFrame.style.top = mypos + "px";
+            animateFrame.style.left = mypos + "px";
+        }
+        else {
+            clearInterval(startChange);
+        }
+    }
+}
