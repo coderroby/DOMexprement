c132291d8d32e4b6c580cdaf03ce716e2d466c65
Md. arifur Rahman <coder.roby@gmail.com>
2021-12-30 02:38:59 +0600

    learn about DOM nodes

--- js/domNodes.js
+++ js/domNodes.js
@@ -2,4 +2,15 @@ const pera  = document.createElement("p");
 const node = document.createTextNode("Coming From JS");
 pera.appendChild(node); //<p> Coming From JS </p>
 const newNode = document.getElementById("div1");
+console.log(pera);
 newNode.appendChild(pera);
+const firstNode = document.getElementById('p1');
+//const firstNode = document.getElementById('div1').firstChild;
+//const firstNode = document.getElementById('div1').childNodes[1];
+
+console.log(firstNode);
+newNode.insertBefore(pera, firstNode);
+const p2 = document.getElementById("p2");
+//p2.remove();
+//p2.removeChild();  //sob browser support kore
+newNode.replaceChild(pera, p2);
